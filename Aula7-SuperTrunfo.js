72cbce51e341fa81dbed2df865867b0a2a6f121b
Jamile-Xavier <jamile.x.m@gmail.com>
2022-09-13 21:59:51 -0300

    atualizacao aula 7

--- Aula7-SuperTrunfo.js
+++ Aula7-SuperTrunfo.js
@@ -1,31 +1,29 @@
-var carta1 = {
-  nome: "Bulbasauro",
-  atributos: {
-    ataque: 7,
-    defesa: 8,
-    magia: 6
+var cartas = [
+  {
+    nome: "Bulbasauro",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 6
+    }
+  },
+  {
+    nome: "Darth Vader",
+    atributos: {
+      ataque: 9,
+      defesa: 8,
+      magia: 2
+    }
+  },
+  {
+    nome: "Shiryu de Dragão",
+    atributos: {
+      ataque: 5,
+      defesa: 9,
+      magia: 10
+    }
   }
-};
-
-var carta2 = {
-  nome: "Darth Vader",
-  atributos: {
-    ataque: 9,
-    defesa: 8,
-    magia: 2
-  }
-};
-
-var carta3 = {
-  nome: "Shiryu de Dragão",
-  atributos: {
-    ataque: 5,
-    defesa: 9,
-    magia: 10
-  }
-};
-
-var cartas = [carta1, carta2, carta3];
+]
 var cartaMaquina = 0;
 var cartaJogador = 0;
 
